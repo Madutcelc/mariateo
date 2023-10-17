bc925835ec2086b97cc42bd5e40568bfe214d022
Madutcelc <142222925+Madutcelc@users.noreply.github.com>
2023-10-17 07:43:12 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 function tocaSomPom() {
   document.querySelector('som_tecla_pom').play();
 }
-document.querySelector('tecla_pom').onclick = ('.tecla_pom');
+document.querySelector('tecla_pom').onclick = ('.tecla');
+
+
+const listaDeTeclas = document.querrySelectorAll('.tecla_pom');
+listaDeTeclas{0}.onclick = tocaSomPom
